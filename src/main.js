8d81c49dbd81e8a2523de3bed69d3ea7df6586d1
MyeonuK <gusdn0217@gmail.com>
2021-04-28 22:10:23 +0900

    Add files && Update file download module

--- src/main.js
+++ src/main.js
@@ -27,17 +27,10 @@ function showPage(category) {
   myBtn.style.color = "#674e4e";
 }
 
-function mouseOver() {
-  this.style.backgroundColor = "#faebd7";
-  this.style.color = "#674e4e";
-}
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 
-function mouseOut(arr, s) {
-  this.style.backgroundColor = "#674e4e";
-  this.style.color = "#ffffff";
-}
-
-window.onload = function () {
+function addBtnEvent() {
   let homeBtn = document.getElementById("home_btn");
   let programBtn = document.getElementById("program_btn");
   let testBtn = document.getElementById("test_btn");
@@ -51,6 +44,48 @@ window.onload = function () {
     btnArr[i].addEventListener("mouseover", mouseOver);
     btnArr[i].addEventListener("mouseout", mouseOut);
   }
+}
+
+function mouseOver() {
+  this.style.backgroundColor = "#faebd7";
+  this.style.color = "#674e4e";
+}
+
+function mouseOut(arr, s) {
+  this.style.backgroundColor = "#674e4e";
+  this.style.color = "#ffffff";
+}
+
+window.onload = function () {
+  addBtnEvent();
 };
 
-let status = 0;
+function loadFile(filePath) {
+  let a;
+  fetch(filePath)
+    .then((response) => response.json())
+    .then((data) => addPaper(data));
+}
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+function addPaper(data) {
+  let parent = document.getElementById("paper_container");
+  console.log(parent);
+
+  for (let file in data) {
+    let temp = document.createElement("div");
+    temp.setAttribute("class", "item");
+
+    temp.innerHTML = `<p class="title"><a href="${file}" download>${data[file][0]}</a></p>
+    <p>${data[file][1]}</p>`;
+
+    parent.appendChild(temp);
+  }
+}
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+let data = loadFile("/files/index.json");
